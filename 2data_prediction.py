7d037dc9fb644f28ef98d33fb02dbe5481a5abd1
ms23128-iiserm <ankitajha9416@gmail.com>
2025-10-24 23:42:19 +0530

    modified code

--- 2data_prediction.py
+++ 2data_prediction.py
@@ -5,4 +5,13 @@ from sklearn.metrics import mean_squared_error, classification_report, accuracy_
 from xgboost import XGBRegressor, XGBClassifier
 from tqdm import tqdm # Used for progress bar during walk-forward validation
 
+# --- Configuration ---
+INPUT_FILE = 'features_engineered_data.csv' 
+OUTPUT_FILE = 'walk_forward_predictions.csv'
+TARGET_PRICE = 'target' 
+TARGET_DIRECTION = 'direction'
+TRAIN_WINDOW_SIZE = 365 # Start training with 365 days of data (approx 1 year)
+# ---------------------
+
+
 
