0afe10ce0b5b48903a75188a4623b25bfe1b9d50
ms23128-iiserm <ankitajha9416@gmail.com>
2025-10-24 21:41:25 +0530

    configuration

--- stationarity_check.py
+++ stationarity_check.py
@@ -1,3 +1,8 @@
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller
 import numpy as np
+
+# --- Configuration ---
+INPUT_FILE = 'features_engineered_data.csv' 
+PRICE_COLUMN = 'Reliance_Close' 
+# ---------------------
