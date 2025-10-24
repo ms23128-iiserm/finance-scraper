9fe858f29ed0cb5096c2c76eb41534b23dd88605
ms23128-iiserm <ankitajha9416@gmail.com>
2025-10-24 21:42:38 +0530

    ADF test done

--- stationarity_check.py
+++ stationarity_check.py
@@ -6,3 +6,17 @@ import numpy as np
 INPUT_FILE = 'features_engineered_data.csv' 
 PRICE_COLUMN = 'Reliance_Close' 
 # ---------------------
+
+def run_adf_test(series):
+    """
+    Performs the Augmented Dickey-Fuller (ADF) test to check for stationarity.
+    """
+    print('Running Augmented Dickey-Fuller Test...')
+    result = adfuller(series, autolag='AIC')
+    
+    adf_output = pd.Series(result[0:4], index=['Test Statistic', 'p-value', 'Lags Used', 'Observations Used'])
+    for key, value in result[4].items():
+        adf_output[f'Critical Value ({key})'] = value
+    
+    print('\n--- ADF Test Results ---')
+    print(adf_output.to_string())
