7b43090da27a5c3a4b6ac374f7adacc9528d8965
gouravbajar <ms23132@iisermohali.ac.in>
2025-10-25 14:35:55 +0530

    final result file added and libraries imported

---
+++ final_result.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.metrics import mean_squared_error, r2_score
+from statsmodels.tsa.arima.model import ARIMA
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from xgboost import XGBRegressor
+import warnings
+import os 
